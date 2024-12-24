2392004282adeb9f025373a6de0cf8f0f1903d01
Jevgeni Kalinin <jevgeni.kalinin@gmail.com>
2024-12-24 08:31:09 +0200

    added 2nd quad indices

--- endless-terrain/src/test/java/endless/terrain/lod/LodLevelTest.java
+++ endless-terrain/src/test/java/endless/terrain/lod/LodLevelTest.java
@@ -18,7 +18,7 @@ public class LodLevelTest {
     int size = baseQuads / partition;
     logger.debug("base quads = {}, partitioned quads (size) = {}, partition = {}", baseQuads, size, partition);
     
-    int indexSize = size * size * 4;
+    int indexSize = size * size * 2 * 3;
     logger.debug("quad index size = {}", indexSize);
     
     List<Integer> indices = new ArrayList<>(indexSize);
@@ -28,9 +28,16 @@ public class LodLevelTest {
       for (int col = 0; col < size; col++) {
         int q0 = row * baseQuads * partition + col * partition;
         int q1 = q0 + (partition - 1) * baseQuads + partition - 1;
-        int q2 = q0 + partition - 1; // TODO -1 ???
-        logger.debug("\tcol = {}, (q0={},q1={},q2={})", col, q0, q1, q2);
-        indices.addAll(List.of(q0, q1, q2)); // temporary
+        int q2 = q0 + partition - 1;
+        
+        indices.addAll(List.of(q0, q1, q2)); 
+        
+        int q3 = q0 + (partition - 1) * baseQuads;
+        logger.debug("\tcol = {}, (q0={},q1={},q2={}) (q0={},q3={},q1={})", col, q0, q1, q2, q0, q3, q1);
+        
+        indices.addAll(List.of(q0, q3, q1));
+        
+        // TODO quad index to triangle index
       }
     }
     
@@ -38,24 +45,26 @@ public class LodLevelTest {
   };
   
   @Test
+  // TODO remove magic numbers
   void test_reindex_8x8() {
     List<Integer> by2 = reindex.apply(8, 2);
-    assertEquals(4 * 4 * 3, by2.size());
+    assertEquals(4 * 4 * 3 * 2, by2.size());
     
     List<Integer> by4 = reindex.apply(8, 4);
-    assertEquals(2 * 2 * 3, by4.size());
+    assertEquals(2 * 2 * 3 * 2, by4.size());
     
     List<Integer> by8 = reindex.apply(8, 8);
-    assertEquals(3, by8.size());
+    assertEquals(3 * 2, by8.size());
   }
   
   @Test
+  // TODO remove magic numbers
   void test_reindex_4x4() {
     List<Integer> by2 = reindex.apply(4, 2);
-    assertEquals(4 * 3, by2.size());
+    assertEquals(4 * 3 * 2, by2.size());
     
     List<Integer> by4 = reindex.apply(4, 4);
-    assertEquals(3, by4.size());
+    assertEquals(3 * 2, by4.size());
   }
   
 }
