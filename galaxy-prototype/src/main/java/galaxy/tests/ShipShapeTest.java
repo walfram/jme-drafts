da9b83f4a8a8fe9e55e086beb3ceddb05da0ceb4
Jevgeni Kalinin <jevgeni.kalinin@gmail.com>
2025-01-29 21:58:37 +0200

    experimenting with ship shapes

--- galaxy-prototype/src/main/java/galaxy/tests/ShipShapeTest.java
+++ galaxy-prototype/src/main/java/galaxy/tests/ShipShapeTest.java
@@ -1,39 +1,14 @@
 package galaxy.tests;
 
-import cells.Cell;
-import cells.Cell2d;
 import com.jme3.app.SimpleApplication;
-import com.jme3.input.event.MouseButtonEvent;
 import com.jme3.material.Material;
 import com.jme3.scene.Geometry;
 import com.jme3.scene.Mesh;
-import com.jme3.scene.Mesh.Mode;
-import com.jme3.scene.Spatial;
 import com.jme3.scene.shape.Cylinder;
 import com.jme3.scene.shape.Torus;
 import com.jme3.system.AppSettings;
-import com.simsilica.lemur.GuiGlobals;
-import com.simsilica.lemur.event.DefaultMouseListener;
-import com.simsilica.lemur.event.MouseEventControl;
 import debug.QuickSetup;
-import java.util.Collection;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Set;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
-import jme3utilities.MyMesh;
-import jme3utilities.mesh.Cone;
-import jme3utilities.mesh.Dodecahedron;
-import jme3utilities.mesh.Icosahedron;
-import jme3utilities.mesh.Icosphere;
-import jme3utilities.mesh.Octahedron;
-import jme3utilities.mesh.Octasphere;
-import jme3utilities.mesh.Prism;
-import jme3utilities.mesh.Tetrahedron;
 import mesh.FlatShadedMesh;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class ShipShapeTest extends SimpleApplication {
 
@@ -47,71 +22,29 @@ public class ShipShapeTest extends SimpleApplication {
     app.start();
   }
 
-  private static final Logger logger = LoggerFactory.getLogger(ShipShapeTest.class);
-
   @Override
   public void simpleInitApp() {
     new QuickSetup().applyTo(this);
-    GuiGlobals.initialize(this);
 
     Material material = new Material(assetManager, "Common/MatDefs/Misc/ShowNormals.j3md");
-
-    List<Mesh> meshes = List.of(
-        new Cone(7, 10, 10, true),
-        new Dodecahedron(10, Mode.Triangles),
-        new Icosahedron(10, true),
-        new Icosphere(1, 10),
-        new Octahedron(10, true),
-        new Octasphere(1, 10),
-        new Prism(7, 10, 10, true),
-        new Tetrahedron(10, true),
-        new Cylinder(5, 7, 10, 10, true),
-        new Torus(5, 7, 5, 10)
-    );
-
-    Cell origin = new Cell2d(0, 0, 32);
-
-    Set<Cell> cells = Stream.of(
-            origin.neighboursAll(),
-            new Cell2d(0, -1, 32).neighboursAll()
-        )
-        .flatMap(Collection::stream)
-        .collect(Collectors.toSet());
+    // material.getAdditionalRenderState().setWireframe(true);
     
-    logger.debug("cells = {}", cells.size());
+    Geometry body = new Geometry("body", new FlatShadedMesh(new Cylinder(3, 3, 5, 40, true)));
+    body.setMaterial(material);
+    rootNode.attachChild(body);
 
-    Iterator<Cell> iterator = cells.iterator();
-
-    for (Mesh mesh : meshes) {
-      boolean hasNormals = MyMesh.hasAnyNormals(mesh);
-      logger.debug("mesh {}, normals = {}", mesh.getClass().getSimpleName(), hasNormals);
-
-      Geometry geometry = new Geometry(mesh.getClass().getSimpleName(), new FlatShadedMesh(mesh));
-      geometry.setMaterial(material);
-
-      if ("Torus".equals(mesh.getClass().getSimpleName()) || "Cylinder".equals(mesh.getClass().getSimpleName())) {
-        geometry.scale(1, 1, 10);
-      }
-      
-      Cell c = iterator.next();
-      geometry.setLocalTranslation(c.translation());
-
-      rootNode.attachChild(geometry);
-
-      MouseEventControl.addListenersToSpatial(geometry, new DefaultMouseListener() {
-        @Override
-        protected void click(MouseButtonEvent event, Spatial target, Spatial capture) {
-          logger.debug("click on {}", target);
-        }
-      });
-    }
-
-    //    Geometry cone = new Geometry("cone", new Cone(7, 10, 10, true));
-    //    cone.setMaterial(material);
-    //    rootNode.attachChild(cone);
-
-    //    Geometry cylinder = new Geometry("hull", new Cylinder(5, 7, 1, 1, true));
-    //    cylinder.setMaterial(material);
-    //    rootNode.attachChild(cylinder);
+    Geometry geometry = new Geometry("", new FlatShadedMesh(new Cylinder(4, 6, 10, 5, true)));
+    geometry.setMaterial(material);
+    geometry.scale(0.75f, 1.25f, 1f);
+    rootNode.attachChild(geometry);
   }
+
+
+//  Geometry torus = new Geometry("", new FlatShadedMesh(new Torus(8, 4, 5, 20)));
+//    torus.setMaterial(material);
+//    rootNode.attachChild(torus);
+//
+//  Geometry cylinder = new Geometry("", new FlatShadedMesh(new Cylinder(4, 8, 10, 60, true)));
+//    cylinder.setMaterial(material);
+//    rootNode.attachChild(cylinder);
 }
