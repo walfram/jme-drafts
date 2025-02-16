2072e218b9c6c20e0c31649ff801ea8b43d24bcd
Jevgeni Kalinin <jevgeni.kalinin@gmail.com>
2025-02-16 17:04:35 +0200

    added generic cargo batches for ship spatial

--- galaxy-prototype/src/main/java/galaxy/ship/designer/GeneratedShip.java
+++ galaxy-prototype/src/main/java/galaxy/ship/designer/GeneratedShip.java
@@ -7,6 +7,7 @@ import cells.Cell2d;
 import com.jme3.material.Material;
 import com.jme3.scene.Geometry;
 import com.jme3.scene.Node;
+import com.jme3.scene.debug.WireBox;
 import com.jme3.scene.shape.Cylinder;
 import galaxy.domain.ship.ShipDesign;
 import jme3utilities.mesh.Icosphere;
@@ -17,8 +18,10 @@ import org.slf4j.LoggerFactory;
 public class GeneratedShip {
 
   private static final Logger logger = LoggerFactory.getLogger(GeneratedShip.class);
-  
+
   private static final float cellExtent = 4f;
+  private static final float containerBatchExtent = 0.95f * cellExtent;
+
   private final ShipDesign design;
   private final Material material;
 
@@ -31,7 +34,7 @@ public class GeneratedShip {
     Node root = new Node("ship-root");
 
     attachHull(root);
-    
+
     if (design.drives().size() > 0) {
       attachDrives(root);
     }
@@ -39,24 +42,24 @@ public class GeneratedShip {
     if (design.cargo().volume() > 0) {
       attachCargo(root);
     }
-    
+
     if (design.weapons().guns() > 0) {
       attachWeapons(root);
     }
-    
+
     if (design.shields().power() > 0) {
       attachShields(root);
     }
-    
+
     return root;
   }
 
   private void attachShields(Node root) {
-    
+
   }
 
   private void attachWeapons(Node root) {
-    
+
   }
 
   private void attachHull(Node root) {
@@ -71,29 +74,40 @@ public class GeneratedShip {
     // count containers
     int containers = (int) ceil(design.cargo().volume());
     // create container batches
-    int batches = containers / 5;
+    int batches = (int) ceil(containers * 0.2f);
+
     logger.debug("containers = {}, batches = {}", containers, batches);
+
+    Node cargo = new Node("cargo");
+    root.attachChild(cargo);
+
+    for (int z = 1; z < 1 + batches; z++) {
+      Geometry batch = new Geometry("batch-%s".formatted(z), new WireBox(containerBatchExtent, containerBatchExtent, containerBatchExtent));
+      batch.setMaterial(material);
+      batch.setLocalTranslation(new Cell2d(0, z, cellExtent).translation());
+      cargo.attachChild(batch);
+    }
   }
 
   private void attachDrives(Node root) {
     Node engine = new Node("engine");
     root.attachChild(engine);
-    
-//    Geometry hullBase = new Geometry("hull-base", new FlatShadedMesh(new Cylinder(2, 8, 3f * cellExtent, 2f * cellExtent, true)));
-//    hullBase.setMaterial(material);
-//    engine.attachChild(hullBase);
-//    hullBase.move(new Cell2d(0, 0, cellExtent).translation());
-//    hullBase.scale(1, 0.5f, 1);
 
-//    Geometry engineCenter = new Geometry("engine-center", new FlatShadedMesh(new Cylinder(2, 8, cellExtent, 0.25f * cellExtent, true)));
-//    engineCenter.setMaterial(material);
-//    engineCenter.setLocalTranslation(new Cell2d(0, -1, cellExtent).translation().add(0, 0, (0.75f + 0.125f) * cellExtent));
-//    engine.attachChild(engineCenter);
-    
+    //    Geometry hullBase = new Geometry("hull-base", new FlatShadedMesh(new Cylinder(2, 8, 3f * cellExtent, 2f * cellExtent, true)));
+    //    hullBase.setMaterial(material);
+    //    engine.attachChild(hullBase);
+    //    hullBase.move(new Cell2d(0, 0, cellExtent).translation());
+    //    hullBase.scale(1, 0.5f, 1);
+
+    //    Geometry engineCenter = new Geometry("engine-center", new FlatShadedMesh(new Cylinder(2, 8, cellExtent, 0.25f * cellExtent, true)));
+    //    engineCenter.setMaterial(material);
+    //    engineCenter.setLocalTranslation(new Cell2d(0, -1, cellExtent).translation().add(0, 0, (0.75f + 0.125f) * cellExtent));
+    //    engine.attachChild(engineCenter);
+
     Geometry engineCenter = new Geometry("engine-center", new FlatShadedMesh(new Cylinder(2, 8, 0.5f * cellExtent, cellExtent, 2f * cellExtent, true, false)));
     engineCenter.setMaterial(material);
     engine.attachChild(engineCenter);
-    
+
     engine.move(new Cell2d(0, -1, cellExtent).translation());
   }
 }
