6f393145ed72372e6c5744afbd63687bf0e0a852
Jevgeni Kalinin <jevgeni.kalinin@gmail.com>
2025-02-16 14:33:48 +0200

    added editing to SpinnerWidget

--- galaxy-prototype/src/main/java/galaxy/ship/designer/widgets/SpinnerWidget.java
+++ galaxy-prototype/src/main/java/galaxy/ship/designer/widgets/SpinnerWidget.java
@@ -1,18 +1,25 @@
 package galaxy.ship.designer.widgets;
 
+import com.jme3.input.event.MouseButtonEvent;
 import com.jme3.scene.Spatial;
 import com.simsilica.lemur.Axis;
 import com.simsilica.lemur.Button;
+import com.simsilica.lemur.Button.ButtonAction;
 import com.simsilica.lemur.Container;
 import com.simsilica.lemur.FillMode;
+import com.simsilica.lemur.GuiGlobals;
 import com.simsilica.lemur.HAlignment;
 import com.simsilica.lemur.Label;
+import com.simsilica.lemur.Panel;
 import com.simsilica.lemur.SequenceModel;
+import com.simsilica.lemur.ValueEditors;
 import com.simsilica.lemur.component.SpringGridLayout;
 import com.simsilica.lemur.core.VersionedReference;
 import com.simsilica.lemur.event.CursorEventControl;
 import com.simsilica.lemur.event.CursorMotionEvent;
 import com.simsilica.lemur.event.DefaultCursorListener;
+import com.simsilica.lemur.event.DefaultMouseListener;
+import com.simsilica.lemur.value.TextFieldValueEditor;
 import jme3utilities.SimpleControl;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -23,15 +30,18 @@ public class SpinnerWidget<T extends Number> extends Container {
   
   private final SequenceModel<T> model;
   private final VersionedReference<T> reference;
+  private final Label label;
+  
+  private TextFieldValueEditor<Double> editor;
 
   public SpinnerWidget(SequenceModel<T> model) {
-    super(new SpringGridLayout(Axis.Y, Axis.X, FillMode.Even, FillMode.First));
+    super(new SpringGridLayout(Axis.Y, Axis.X, FillMode.ForcedEven, FillMode.First));
 
     this.reference = model.createReference();
     this.model = model;
-
-    Label label = addChild(new Label("%.02f".formatted(model.getObject().doubleValue())));
-
+    
+    label = addChild(new Label("%.02f".formatted(model.getObject().doubleValue())));
+    
     Button decrease = addChild(new Button("-"), 1);
     decrease.setMaxWidth(32f);
     decrease.setTextHAlignment(HAlignment.Center);
@@ -41,7 +51,7 @@ public class SpinnerWidget<T extends Number> extends Container {
     increase.setMaxWidth(32f);
     increase.setTextHAlignment(HAlignment.Center);
     increase.addClickCommands(b -> model.setObject(model.getNextObject()));
-
+    
     addControl(new SimpleControl() {
       @Override
       protected void controlUpdate(float updateInterval) {
@@ -66,6 +76,38 @@ public class SpinnerWidget<T extends Number> extends Container {
         }
       }
     });
+    
+    addMouseListener(new DefaultMouseListener() {
+      @Override
+      protected void click(MouseButtonEvent event, Spatial target, Spatial capture) {
+        getLayout().removeChild(label);
+
+        editor = ValueEditors.doubleEditor("%.02f");
+        editor.startEditing((Double) model.getObject());
+        
+        Panel panel = editor.getEditor();
+        
+        getLayout().addChild(panel, 0, 0);
+        GuiGlobals.getInstance().requestFocus(panel);
+      }
+    });
+  }
+
+  @Override
+  public void updateLogicalState(float tpf) {
+    super.updateLogicalState(tpf);
+    
+    if (editor != null) {
+      if (!editor.updateState(tpf)) {
+        model.setObject((T) editor.getObject());
+        
+        getLayout().removeChild(editor.getEditor());
+        getLayout().addChild(label, 0, 0);
+        GuiGlobals.getInstance().requestFocus(label);
+        
+        editor = null;
+      }
+    }
   }
 
   public SequenceModel<T> model() {
