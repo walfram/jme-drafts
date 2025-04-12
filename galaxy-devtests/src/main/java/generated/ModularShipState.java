4b2e5294b04a9b44b8d0c115f8157124e4eaf529
Jevgeni Kalinin <jevgeni.kalinin@gmail.com>
2025-04-12 20:47:21 +0300

    experimenting

--- galaxy-devtests/src/main/java/generated/ModularShipState.java
+++ galaxy-devtests/src/main/java/generated/ModularShipState.java
@@ -9,10 +9,11 @@ import com.jme3.scene.Geometry;
 import com.jme3.scene.Mesh;
 import com.jme3.scene.Node;
 import com.jme3.scene.shape.Cylinder;
+import com.simsilica.lemur.geom.DMesh;
+import com.simsilica.lemur.geom.Deformation;
 import com.simsilica.lemur.geom.MBox;
 import materials.ShowNormalsMaterial;
 import mesh.FlatShadedMesh;
-import tests.ScaleUpTo;
 
 public class ModularShipState extends BaseAppState {
   
@@ -29,6 +30,29 @@ public class ModularShipState extends BaseAppState {
   protected void initialize(Application app) {
     material = new ShowNormalsMaterial(app.getAssetManager());
     
+    Geometry bridge = new Geometry("bridge", new FlatShadedMesh(new Cylinder(2, 6, 0.5f, 1f, 1, true, false)));
+    bridge.setMaterial(material);
+    scene.attachChild(bridge);
+    bridge.scale(cellExtent, 0.5f * cellExtent, 2f * cellExtent);
+    bridge.move(0, 0, 3f * 2f * cellExtent);
+    
+    Deformation transform = (v, n) -> {
+      if (v.y > 0)
+        v.y = 0;
+    };
+    Geometry hull = new Geometry("hull", new FlatShadedMesh(new DMesh(new Cylinder(2, 6, 1, 1, true), transform)));
+    hull.setMaterial(material);
+    scene.attachChild(hull);
+    hull.scale(cellExtent, 0.5f * cellExtent, 5f * 2f * cellExtent);
+    
+    Geometry engine = new Geometry("engine", new FlatShadedMesh(new Cylinder(2, 6, 1, 1, true)));
+    engine.setMaterial(material);
+    scene.attachChild(engine);
+    engine.scale(1.25f * cellExtent, 1.25f * cellExtent, 2f * cellExtent);
+    engine.move(0, 0, -3f * 2f * cellExtent);
+  }
+  
+  private void cylinder() {
     Mesh cylinder = new FlatShadedMesh(new Cylinder(2, 6, 1, 1, true));
     Geometry geometry = new Geometry("cylinder", cylinder);
     geometry.setMaterial(material);
