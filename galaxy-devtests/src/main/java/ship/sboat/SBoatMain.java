47e425036b5329866dafb36d53f37853ac5173a8
Jevgeni Kalinin <jevgeni.kalinin@gmail.com>
2025-04-28 20:56:53 +0300

    probably semi-final version of ship design

---
+++ galaxy-devtests/src/main/java/ship/sboat/SBoatMain.java
@@ -0,0 +1,35 @@
+package ship.sboat;
+
+import com.jme3.app.SimpleApplication;
+import com.jme3.system.AppSettings;
+import debug.QuickAppSettings;
+import ship.common.CameraState;
+import ship.common.DebugAxesState;
+import ship.common.DebugGridState;
+import ship.common.LemurState;
+
+public class SBoatMain extends SimpleApplication {
+  
+  public static void main(String[] args) {
+    AppSettings settings = new QuickAppSettings().settings();
+    
+    SBoatMain app = new SBoatMain();
+    app.setSettings(settings);
+    app.setShowSettings(false);
+    
+    app.start();
+  }
+  
+  @Override
+  public void simpleInitApp() {
+    final float cellExtent = 4f;
+    
+    stateManager.attach(new DebugGridState(rootNode, cellExtent));
+    stateManager.attach(new DebugAxesState(rootNode));
+    stateManager.attach(new CameraState());
+    
+    stateManager.attach(new LemurState());
+    
+    stateManager.attach(new SBoatState(rootNode));
+  }
+}
